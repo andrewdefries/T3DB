2c884cd3836b311fdb9b62a9e1aecb5732b0bd02
andrewdefries <ajd@backmandefries.com>
2014-04-14 10:54:35 -0700

    changed tree ps1 ready

---
+++ ProblemSet/T3DB_PS1.mm
@@ -0,0 +1,15 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1397497599492" ID="ID_129583136" MODIFIED="1397497614092" TEXT="T3DB">
+<node CREATED="1397497614881" ID="ID_1360488681" MODIFIED="1397497624475" POSITION="right" TEXT="Target Toxin Database">
+<node CREATED="1397497628370" ID="ID_741719956" MODIFIED="1397497689217" TEXT="targets.csv">
+<node CREATED="1397497702608" ID="ID_733128529" MODIFIED="1397497713321" TEXT="extract uniprot id"/>
+<node CREATED="1397497713884" ID="ID_1534491989" MODIFIED="1397497956790" TEXT="biomaRt: convert uniprot id to ensemble">
+<node CREATED="1397497728208" ID="ID_1671142862" MODIFIED="1397497970330" TEXT="Inparanoid: use ensemble id to query inparanoid">
+<node CREATED="1397497974143" ID="ID_797784809" MODIFIED="1397498014586" TEXT="bioassayR: send candidate genes for chemical and target query"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
